dfbfacdff5139130045264d874c213e07aceb45f
hiep-tm <tranhiep.az2@gmail.com>
2023-09-08 17:07:08 +0700

    first commit by hiep

---
+++ src/com/fu/gitwelcome/GitWelcomeTeam.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.fu.gitwelcome;
+
+/**
+ *
+ * @author W10
+ */
+public class GitWelcomeTeam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("llll");
+        System.out.println("llll");
+        System.out.println("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+        for (int i = 0; i < 100000; i++) {
+            System.out.println("gggg");
+        }
+    }
+}
